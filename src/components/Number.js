b6a799d45b9c20074f84ca48128afec6206aad8a
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2019-10-03 12:54:47 +0800

    further destructure

--- src/components/Number.js
+++ src/components/Number.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-import './Number.css';
+import "./Number.css";
 
-const Number = props => {
-  return (
+const Number = ({ number }) => {
+	return (
 		<div className="text-white d-flex justify-content-end align-items-center bg-dark-blue mb-2">
-			<h1 className="mr-3 mb-0">{props.number}</h1>
+			<h1 className="mr-3 mb-0">{number}</h1>
 		</div>
 	);
-}
+};
 
 export default Number;
